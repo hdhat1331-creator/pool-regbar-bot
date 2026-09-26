aa19ea1c741969182f9e74176f72d0b2aa71d633
hdhat1331-creator <hdhat1331@gmail.com>
2026-09-26 22:34:06 +0330

    Create states.py

---
+++ states.py
@@ -0,0 +1,13 @@
+WAITING_FOR_INVITE_NUMBER = "waiting_for_invite_number"
+WAITING_FOR_MESSAGE = "waiting_for_message"
+WAITING_FOR_TICKET = "waiting_for_ticket"
+
+ADMIN_BAN_USER = "admin_ban_user"
+ADMIN_GRANT_GOLD = "admin_grant_gold"
+ADMIN_BROADCAST = "admin_broadcast"
+ADMIN_BLOCK_USER = "admin_block_user"
+ADMIN_GOLD_MESSAGE = "admin_gold_message"
+ADMIN_TICKET_REPLY = "admin_ticket_reply"
+
+user_states = {}
+user_data = {}
